481721168a253b91868c22e27ce56d08ef4da403
Richard Abear <owchzzz@gmail.com>
2018-08-05 21:59:45 +0800

    Return error

--- src/Auth/index.js
+++ src/Auth/index.js
@@ -36,7 +36,7 @@ router.post('/login', (req, res) => {
             password: req.body.password
         },{$set:{token:nanoid()}}, {new:true}, (err, result) => {
             if (err || result == null) {
-                res.json(err || {message:'User does not exist'});
+                res.json({message:'User does not exist'});
                 res.end();
                 return false;
             }
