cbca47785bc87c4c6932bdd4ef24995019454852
Richard Abear <owchzzz@gmail.com>
2018-08-12 16:53:32 +0800

    sample

--- src/Middleware/authenticated.js
+++ src/Middleware/authenticated.js
@@ -5,7 +5,7 @@ module.exports = ((opts) => {
     return (req, res, next) => {
         console.log('Request came in for authentication');
         const UserModel=mongoose.model('UserModel');
-        debug.log(req.headers);
+        console.log(req.headers);
         if(req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
             let token = req.headers.authorization.split(' ')[1];
             console.log("Token Request:",token);
