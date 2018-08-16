bfcf8a71cc5c52e669688c815c2f1989f7135ba2
Richard Abear <owchzzz@gmail.com>
2018-08-16 16:18:39 +0800

    Server

--- src/Actions/user.js
+++ src/Actions/user.js
@@ -7,7 +7,19 @@ router.use(require('../Middleware/authenticated'));
 router.post('/update/items',(req, res) => {
     const UserModel = mongoose.model('UserModel');
     console.log(req.body);
-    res.json({message:'successfully queried json with obj',items: req.user.items});
+    let items = [];
+    if(req.user.items.length == 0) {
+        items = [
+            {name: 'razor', qty: 0},
+            {name: 'pen', qty: 3},
+            {name: 'marker', qty: 0}
+        ]
+
+    } 
+    else {
+        items = req.user.items;
+    }
+    res.json({message:'successfully queried json with obj',items});
 });
 
 
