7201ed24fb18136da2eea55ebc861f1f2879a30a
Richard Abear <owchzzz@gmail.com>
2018-08-16 17:23:33 +0800

    Recieve json

--- src/Actions/user.js
+++ src/Actions/user.js
@@ -6,14 +6,30 @@ router.use(require('../Middleware/authenticated'));
 
 router.post('/update/items',(req, res) => {
     const UserModel = mongoose.model('UserModel');
-    console.log("requested update to user items:",req.user.items);
+    console.log("requested update to user items:",req.body.data);
 
     let items = [];
     if(req.user.items.length == 0 || ! req.user.items) {
         items = [
             {name: 'razor', qty: 0},
             {name: 'pen', qty: 3},
-            {name: 'marker', qty: 0}
+            {name: 'marker', qty: 0},
+            {name: 'highlighter', qty: 0},
+            {name: 'laptop', qty: 0},
+            {name: 'cellphone', qty: 0},
+            {name: 'powerbank', qty: 0},
+            {name: 'folder', qty: 0},
+            {name: 'organizer', qty: 0},
+            {name: 'milk', qty: 0},
+            {name: 'flashdrive', qty: 0},
+            {name: 'jacket', qty: 0},
+            {name: 'guitar', qty: 0},
+            {name: 'handbook', qty: 0},
+            {name: 'umbrella', qty: 0},
+            {name: 'yearbook', qty: 0},
+            {name: 'questionnaire', qty: 0},
+            {name: 'hairwax', qty: 0},
+            {name: 'textbook', qty: 0},
         ]
         UserModel.findOneAndUpdate({_id:req.user._id}, {$set:{items}},{new:true}, (err, doc) => {
             res.json(doc);
